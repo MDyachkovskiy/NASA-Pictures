dcfe790448cfd2c03b76bc0e8e421e4bef619db8
Mdyachkovskiy <mdyachkovskiy@gmail.com>
2023-03-12 15:04:30 +0500

    HW_5 добавлена анимация поворота плюсика в кнопке

--- app/src/main/java/com/gb_materialdesign/ui/main/space/SpaceFragment.kt
+++ app/src/main/java/com/gb_materialdesign/ui/main/space/SpaceFragment.kt
@@ -1,5 +1,6 @@
 package com.gb_materialdesign.ui.main.space
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,8 @@ class SpaceFragment: Fragment() {
 
     private var _binding: FragmentSpaceBinding? = null
     private val binding get() = _binding!!
+
+    private var isMenuDisplayed = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +33,19 @@ class SpaceFragment: Fragment() {
             .replace(R.id.space_container, AsteroidsListFragment.newInstance())
             .addToBackStack("")
             .commit()
+
+        binding.fab.setOnClickListener {
+            isMenuDisplayed = !isMenuDisplayed
+            if(isMenuDisplayed) {
+                ObjectAnimator.ofFloat(binding.plusFab, View.ROTATION, 0f, 360f)
+                    .setDuration(1000)
+                    .start()
+            } else {
+                ObjectAnimator.ofFloat(binding.plusFab, View.ROTATION, 0f, -360f)
+                    .setDuration(1000)
+                    .start()
+            }
+        }
     }
 
     override fun onDestroyView() {
